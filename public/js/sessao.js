e086115240afb1bf41c450dd85549655895d0e02
gabrieldmeloSptech <gabriel.dmelo@sptech.school>
2026-04-26 00:39:34 -0300

    Web data viz no site login e cadastro funcionando

---
+++ public/js/sessao.js
@@ -0,0 +1,29 @@
+// sessão
+function validarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    var b_usuario = document.getElementById("b_usuario");
+
+    if (email != null && nome != null) {
+        b_usuario.innerHTML = nome;
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+
+function finalizarAguardar(texto) {
+    var divErrosLogin = document.getElementById("mensagem");
+    if (texto) {
+        divErrosLogin.style.display = "flex";
+        divErrosLogin.innerHTML = texto;
+    }
+}
+
---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,10 @@
+create database Projeto;
+
+use Projeto;
+
+create table Bd (
+id int primary key auto_increment,
+Nome varchar(45),
+email varchar(45)
+);
+
